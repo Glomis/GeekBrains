055a397f279ac89ae858df82a5ecdb11455d95a3
Maxim <55534590+Glomis@users.noreply.github.com>
2020-08-04 00:13:04 +0300

    Created Queue

--- HW || Mityushin Max/main.swift
+++ HW || Mityushin Max/main.swift
@@ -10,3 +10,43 @@ import Foundation
 
 print("Hello, World!")
 
+protocol Car {
+    var brand: String { get }
+    var productionYear: Int { get }
+}
+
+class SportCar: Car {
+    var brand: String
+    var productionYear: Int
+    
+    init(brand: String, productionYear: Int) {
+        self.brand = brand
+        self.productionYear = productionYear
+    }
+}
+
+class TrunkCar: Car {
+    var brand: String
+    var productionYear: Int
+    
+    init(brand: String, productionYear: Int) {
+        self.brand = brand
+        self.productionYear = productionYear
+    }
+}
+
+struct Queue<T> {
+    private var elements: [T] = []
+    
+    mutating func push(newCar: T) {
+        elements.append(newCar)
+    }
+    
+    mutating func pop() -> T? {
+        guard !elements.isEmpty else { return nil }
+        return elements.removeFirst()
+    }
+}
+
+
+
