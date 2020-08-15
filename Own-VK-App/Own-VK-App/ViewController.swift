074a2964b8ecc05e53fe13a437867dccefc9ad92
Maxim <55534590+Glomis@users.noreply.github.com>
2020-08-15 14:27:05 +0300

    initial commit

---
+++ Own-VK-App/Own-VK-App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Own-VK-App
+//
+//  Created by Tatyana on 15.08.2020.
+//  Copyright © 2020 Max&Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //IBOutlets
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var enterBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    @IBAction func enterBtnTapped(_ sender: Any) {
+        
+    }
+}
